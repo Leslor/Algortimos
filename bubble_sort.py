de39fbc735b2403a55164f0608bdd23e636df742
Leslor_P <leslie.paz.am@gmail.com>
2020-09-22 11:58:45 -0500

    Se agrega el algoritmo de ordenamiento bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+import random
+
+def buble_sort(lista):
+    n=len(lista)
+    for i in range(n):
+        print(lista)
+        cambios=False
+        for j in range(0,n-i-1):
+            if lista[j]>lista[j+1]:
+                lista[j],lista[j+1]= lista[j+1],lista[j]
+                cambios=True
+        if not cambios:
+            break
+
+lista=[random.randint(1,20) for i in range(10) ]
+buble_sort(lista)
